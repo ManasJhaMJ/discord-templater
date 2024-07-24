b1bf272917924ad45e54b9fe3118bdf8f454feb9
manasjhamj <jhamanas939@gmail.com>
2024-07-24 23:20:36 +0530

    connect with firebase

--- src/components/ServerTemplate/ServerTemplate.jsx
+++ src/components/ServerTemplate/ServerTemplate.jsx
@@ -1,20 +1,65 @@
-import templatedata from '../../Data/ServerTemplate.json'
-import TemplateCard from './TemplateCard'
-import './serverTemplate.css'
+// import templatedata from '../../Data/ServerTemplate.json'
+// import TemplateCard from './TemplateCard'
+// import './serverTemplate.css'
+
+// function ServerTemplate() {
+//     return (
+//         <section id='templates' className='wrapper'>
+//             <h1>Server Templates</h1>
+//             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident possimus corrupti non, praesentium optio, pariatur iure illo eveniet consectetur nostrum reiciendis quae doloremque dolore natus fugiat itaque voluptatibus debitis. Quasi.</p>
+
+//             <div className="templates">
+//                 {templatedata.map((template) => (
+//                     <TemplateCard key={template.id} template={template} />
+//                 ))}
+//             </div>
+//         </section>
+//     )
+// }
+
+// export default ServerTemplate
+
+import app from '../../firebase';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import TemplateCard from './TemplateCard';
+import './serverTemplate.css';
+import { useState, useEffect } from 'react';
 
 function ServerTemplate() {
+    const [serverData, setServerData] = useState([]);
+
+    useEffect(() => {
+        const db = getDatabase(app);
+        const serverRef = ref(db, '/ServerTemplate');
+
+        // Listen for real-time updates
+        const unsubscribe = onValue(serverRef, (snapshot) => {
+            if (snapshot.exists()) {
+                setServerData(Object.values(snapshot.val()));
+            } else {
+                alert('No data available');
+            }
+        });
+
+        // Clean up the listener when the component unmounts
+        return () => unsubscribe();
+    }, []);
+
     return (
         <section id='templates' className='wrapper'>
             <h1>Server Templates</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident possimus corrupti non, praesentium optio, pariatur iure illo eveniet consectetur nostrum reiciendis quae doloremque dolore natus fugiat itaque voluptatibus debitis. Quasi.</p>
 
             <div className="templates">
-                {templatedata.map((template) => (
-                    <TemplateCard key={template.id} template={template} />
+                {serverData.map((template) => (
+                    <TemplateCard
+                        key={template.id}
+                        template={template}
+                    />
                 ))}
             </div>
         </section>
-    )
+    );
 }
 
-export default ServerTemplate
+export default ServerTemplate;
--- src/components/ServerTemplate/TemplateCard.jsx
+++ src/components/ServerTemplate/TemplateCard.jsx
@@ -1,6 +1,18 @@
 /* eslint-disable react/prop-types */
-import './serverTemplate.css'
+/* eslint-disable react/prop-types */
+import './serverTemplate.css';
+import app from '../../firebase';
+import { getDatabase, ref, update } from 'firebase/database';
+
 function TemplateCard({ template }) {
+    const handleUseItClick = async () => {
+        const db = getDatabase(app);
+        const templateRef = ref(db, `/ServerTemplate/${template.id}`);
+        await update(templateRef, {
+            uses: template.uses + 1
+        });
+    };
+
     return (
         <div className='col-12 col-md-6 col-lg-4'>
             <div className="template-card">
@@ -10,7 +22,7 @@ function TemplateCard({ template }) {
                     <p className="card-desc">{template.description}</p>
                     <span className='link-uses'>
                         <a href={template.link} target="_blank" rel="noopener noreferrer">
-                            <button className="btn">Use It!</button>
+                            <button className="btn" onClick={handleUseItClick}>Use It!</button>
                         </a>
                         <h5>{template.uses} Uses</h5>
                     </span>
@@ -23,8 +35,9 @@ function TemplateCard({ template }) {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default TemplateCard
+export default TemplateCard;
+
 
